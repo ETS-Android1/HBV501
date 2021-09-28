cb641ed2b188e263e602364b56a1ff62bdfbe645
Anna Jaerving <annajrving@gmail.com>
2021-09-28 13:13:59 +0000

    initial import spam

--- feedme/src/main/java/is/hi/feedme/controllers/RootController.java
+++ feedme/src/main/java/is/hi/feedme/controllers/RootController.java
@@ -1,15 +1,17 @@
 package is.hi.feedme.controllers;
 
+import java.util.*;
+
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.http.MediaType;
+
 
 @RestController
 public class RootController {
-   
-    // @RequestMapping("/")
-    // public String HomeController() {
-    //     return "";
-    // }
-    //
-    // Should describe API structure and routes
+    
+    @RequestMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Map index() {
+        return Collections.singletonMap("TODO", "Find non retarded way for static JSON");
+    }
 }
