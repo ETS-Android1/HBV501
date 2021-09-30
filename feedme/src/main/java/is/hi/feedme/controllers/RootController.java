278f8a3fba90cd6932fd608ce448785530a0ef1d
Anna Jaerving <annajrving@gmail.com>
2021-09-30 09:43:10 +0000

    some formatting

--- feedme/src/main/java/is/hi/feedme/controllers/RootController.java
+++ feedme/src/main/java/is/hi/feedme/controllers/RootController.java
@@ -6,10 +6,9 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.http.MediaType;
 
-
 @RestController
 public class RootController {
-    
+
     @RequestMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
     public Map index() {
         return Collections.singletonMap("TODO", "Find non retarded way for static JSON");
