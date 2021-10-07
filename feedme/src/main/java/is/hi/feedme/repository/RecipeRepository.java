43e6f3ac629646671e2b30ba9ef1027320069361
Anna Jaerving <annajrving@gmail.com>
2021-10-07 10:31:46 +0000

    Crud -> Jpa

--- feedme/src/main/java/is/hi/feedme/repository/RecipeRepository.java
+++ feedme/src/main/java/is/hi/feedme/repository/RecipeRepository.java
@@ -1,10 +1,11 @@
 package is.hi.feedme.repository;
 
 import is.hi.feedme.model.Recipe;
-import org.springframework.data.repository.CrudRepository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RecipeRepository extends CrudRepository<Recipe, Long> {
+public interface RecipeRepository extends JpaRepository<Recipe, Long> {
     Recipe findByName(String name);
 }
--- feedme/src/main/java/is/hi/feedme/repository/UserRepository.java
+++ feedme/src/main/java/is/hi/feedme/repository/UserRepository.java
@@ -1,10 +1,11 @@
 package is.hi.feedme.repository;
 
 import is.hi.feedme.model.User;
-import org.springframework.data.repository.CrudRepository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
     User findByUsername(String username);
 }
