e7af98ec044ed4f740452a091d2da72a73af0984
Anna Jaerving <annajrving@gmail.com>
2021-10-15 15:51:11 +0000

    admin user insert

--- feedme/src/main/resources/data.sql
+++ feedme/src/main/resources/data.sql
@@ -2,3 +2,8 @@ INSERT INTO
   users (username, email, password, admin)
 VALUES
   ('admin', 'bla@bla.bla', '$2b$12$BB1tJYM6EUAaQr1YhslmHOKJkuQrBAxiPqw66pC.PsCtm/pte6uBi', true);
+
+INSERT INTO
+  users (username, email, password, admin)
+VALUES
+  ('johndoe', 'john@bla.bla', '$2b$12$BB1tJYM6EUAaQr1YhslmHOKJkuQrBAxiPqw66pC.PsCtm/pte6uBi', false);
