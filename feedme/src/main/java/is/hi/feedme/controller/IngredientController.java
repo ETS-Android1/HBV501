a850ab9d96810d92c8e4d98b4ce0de57fc05a496
Anna Jaerving <annajrving@gmail.com>
2021-10-19 17:59:41 +0000

    cleaning up ingredient imports

--- feedme/src/main/java/is/hi/feedme/controller/IngredientController.java
+++ feedme/src/main/java/is/hi/feedme/controller/IngredientController.java
@@ -1,18 +1,12 @@
 package is.hi.feedme.controller;
 
 import is.hi.feedme.model.Ingredient;
-import is.hi.feedme.model.Recipe;
-import is.hi.feedme.model.RecipeDto;
-import is.hi.feedme.model.SimplifiedRecipe;
 import is.hi.feedme.service.RecipeService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.security.access.prepost.PreAuthorize;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
