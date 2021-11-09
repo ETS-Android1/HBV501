6c8218c220bf2322c2ba7d1dc59ebb5c56eca8e5
vallzy <45981588+vallzy@users.noreply.github.com>
2021-11-09 20:27:05 +0000

    Frontend - Write review validations
    Several validations placed on review, as well as displaying an error if user has already written a review for the specific recipe.

--- feedme-client/src/views/ViewRecipe.vue
+++ feedme-client/src/views/ViewRecipe.vue
@@ -8,7 +8,7 @@
     >
       <v-card>
         <v-toolbar color="orange darken-1">
-          <v-btn icon dark v-on:click="dialog = false">
+          <v-btn icon dark v-on:click="dialog = false; selectedItemError = { message:'', show:false}">
             <v-icon>mdi-close</v-icon>
           </v-btn>
           <v-toolbar-title style="color: white"
@@ -20,7 +20,6 @@
               dark
               text
               v-on:click="
-                dialog = false;
                 addReview();
               "
             >
@@ -33,30 +32,51 @@
             <v-col cols="12">
               <v-text-field
                 label="Title"
+                :error-messages="titleErrors"
                 :value="selectedItem.title"
                 v-model="selectedItem.title"
                 required
+                @input="$v.selectedItem.title.$touch()"
+                @blur="$v.selectedItem.title.$touch()"
               ></v-text-field>
             </v-col>
             <v-col cols="12">
               <v-textarea
                 counter
                 label="Text"
+                :error-messages="contentErrors"
                 :value="selectedItem.subtitle"
                 v-model="selectedItem.subtitle"
+                required
+                @input="$v.selectedItem.subtitle.$touch()"
+                @blur="$v.selectedItem.subtitle.$touch()"
               ></v-textarea>
             </v-col>
             <v-col cols="12">
               Rate the recipe
               <v-rating
                 v-model="selectedItem.rating"
+                :error-messages="ratingErrors"
                 color="yellow darken-3"
                 background-color="grey darken-1"
                 empty-icon="$ratingFull"
-                half-increments
                 hover
                 large
+                required
+                @input="$v.selectedItem.rating.$touch()"
+                @blur="$v.selectedItem.rating.$touch()"
               ></v-rating>
+            </v-col>
+            <v-col cols="12">
+              <v-alert
+              v-if="selectedItemError.show"
+      outlined
+      type="error"
+      prominent
+      border="left"
+    >
+      {{selectedItemError.message}}
+    </v-alert>
             </v-col>
           </v-row>
         </v-container>
@@ -164,7 +184,12 @@
             </v-card-text>
           </v-card>
           <v-card flat>
-            <v-card-title>Reviews</v-card-title>
+            <v-card-title>
+              Reviews
+              <v-chip class="ma-2" color="green" text-color="white">
+                    {{ recipe.reviews.length }}
+              </v-chip>
+              </v-card-title>
             <v-card-subtitle>Feedback from our users</v-card-subtitle>
             <v-card-text>
               <v-list v-if="reviewList.length > 0" three-line>
@@ -262,17 +287,28 @@
 import { getRecipeById, postReview, deleteReview } from "../service/recipeapi";
 import { initPage, updatePage, pages } from "../misc/pagination";
 import { getUserInfo } from "../service/userapi";
+import { validationMixin } from "vuelidate";
+import { required, maxLength } from "vuelidate/lib/validators";
 export default {
   name: "ViewRecipe",
+  mixins: [validationMixin],
+  validations: {
+    selectedItem: {
+      title: { required, maxLength: maxLength(30) },
+      subtitle: { required, maxLength: maxLength(200) },
+      rating: { required }
+    }
+  },
   data() {
     return {
       recipe: { ingredients: [] },
       dialog: false,
       selectedItem: { title: "", subtitle: "", rating: 0.0 },
+      selectedItemError: { message: "", show: false},
       pageInfo: {
         historyList: [],
         page: 1,
-        pageSize: 2,
+        pageSize: 5,
         listCount: 0,
       },
       reviewList: [],
@@ -297,23 +333,29 @@ export default {
         a.ingredient.name > b.ingredient.name ? 1 : -1
       );
       this.reviewList = this.recipe.reviews;
-      console.log(this.reviewList);
-      console.log(this.$store.state.user);
     },
     async addReview() {
+      this.selectedItemError = { message:'', show:false};
+      this.$v.$touch();
+      if (!this.$v.$anyError) {
+        //can submit
       postReview(this.recipe.id, this.selectedItem)
         .then(async (response) => {
           if (response.status === 201) {
+            this.dialog = false;
             await this.getRecipe();
             this.initPage();
-          }
+          } 
         })
-        .catch((err) => {
-          console.log("err", err);
-          //User already has a review
+        .catch(() => {
+            this.selectedItemError.message = "You can only write one review per recipe. Use edit, or delete your post before writing another one.";
+            this.selectedItemError.show = true;
         });
+      }
     },
     madeReviewOrAdmin(username) {
+      if(this.$store.state.user == undefined || this.$store.state.user == null)
+        return false;
       return (
         this.$store.state.user.username === username ||
         this.$store.state.user.admin
@@ -348,6 +390,28 @@ export default {
     pages() {
       return pages(this.pageInfo);
     },
+    titleErrors() {
+      const errors = [];
+      if (!this.$v.selectedItem.title.$dirty) return errors;
+      !this.$v.selectedItem.title.maxLength &&
+        errors.push("Username must be at most 30 characters long");
+      !this.$v.selectedItem.title.required && errors.push("Title is required.");
+      return errors;
+    },
+    contentErrors() {
+      const errors = [];
+      if (!this.$v.selectedItem.title.$dirty) return errors;
+      !this.$v.selectedItem.title.maxLength &&
+        errors.push("Content must be at most 200 characters long");
+      !this.$v.selectedItem.title.required && errors.push("Content is required.");
+      return errors;
+    },
+    ratingErrors() {
+      const errors = [];
+      if (!this.$v.selectedItem.rating.$dirty) return errors;
+      !this.$v.selectedItem.rating.required && errors.push("Rating is required.");
+      return errors;
+    }
   },
 };
 </script>
