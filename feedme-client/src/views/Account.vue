1f7d73c9ee18c531589bf6303511d267173ea003
vallzy <45981588+vallzy@users.noreply.github.com>
2021-11-11 14:49:44 +0000

    Frontend - Navigation on account page
    Added navigation for the account page, so that the tabs are uniquely identified on the url params. This will allow the router to target your previous tab when traversing to the recipe views from the compact table.

--- feedme-client/src/views/Account.vue
+++ feedme-client/src/views/Account.vue
@@ -11,16 +11,85 @@
             </v-card-actions>
           </v-card>
         </v-dialog>
+        <v-card flat>
+    <v-toolbar
+      flat
+      light
+    >
+      <v-toolbar-title>User Profile</v-toolbar-title>
+    </v-toolbar>
+    <v-tabs v-model="tab" vertical color="orange lighten-1">
+      <v-tab href="#edit">
+        <v-icon left>
+          mdi-account
+        </v-icon>
+        Account
+      </v-tab>
+      <v-tab href="#favorites">
+        <v-icon left>
+          mdi-heart
+        </v-icon>
+        Favorites
+      </v-tab>
+      <v-tab-item value="edit">  
+        <v-card
+        flat
+        class="mx-auto"
+        width="600"
+        height="600"
+        >
+        <v-card-title>
+          Edit your account information
+        </v-card-title>  
+        <v-card-text>
+          <v-card>
+            <v-card-text>
+              <v-text-field
+              v-model="user.username"
+              label="Username (*cant be changed.)"
+              prepend-inner-icon="mdi-account"
+              readonly
+            ></v-text-field>
+            <v-text-field
+              v-model="email"
+              label="Email"
+              prepend-inner-icon="mdi-email"
+            ></v-text-field>
+            <v-text-field
+              v-model="password"
+              label="Current password"
+              prepend-inner-icon="mdi-lock"
+              type="password"
+            ></v-text-field>
+            <v-text-field
+              v-model="newPassword"
+              label="New password"
+              prepend-inner-icon="mdi-lock"
+              type="password"
+            ></v-text-field>
+            </v-card-text>
+            <v-card-actions>
+              <v-btn color="orange darken-1" class="mr-4" width="100%">
+              Update
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-card-text>
+        </v-card>  
+      </v-tab-item>
+      <v-tab-item value="favorites">
         <v-card
         flat
         class="mx-auto"
         width="1000"
+        height="800"
         >
         <v-card-title>
             Your favorite recipes!
         </v-card-title>
         <v-card-text>
         <v-data-table
+        v-if="recipes.length > 0"
         :headers="headers"
         :items="recipes"
         item-key="name"
@@ -34,6 +103,11 @@
             class="mx-4"
             ></v-text-field>
         </template>
+        <template #item.name="{ item }">
+        <router-link :to="{path:'/viewrecipe',query:{id: item.id}}">
+          {{ item.name }}
+        </router-link>
+      </template>
         <template v-slot:body.append>
             <tr>
             <td></td>
@@ -56,8 +130,12 @@
       </v-icon>
     </template>
         </v-data-table>
+        <p v-else class="text--primary">You don't have any recipes in your favorites.</p>
         </v-card-text>
-        </v-card>
+        </v-card>     
+      </v-tab-item>
+    </v-tabs>
+  </v-card>      
     </div>
 </template>
 
@@ -68,7 +146,10 @@ export default ({
     data() {
         return {
             dialogDelete: false,
-            user: null,
+            user: { username: '' },
+            email: '',
+            password: '',
+            newPassword: '',
             recipes: [],
             search: '',
             calories: '',
@@ -123,11 +204,11 @@ export default ({
     async created() {
         const dat = (await getUserInfo()).data;
         this.user = dat.user;
+        this.email = this.user.email;
         this.recipes = dat.recipes;
     },
     async mounted() {
-        const m = await getUserInfo();
-        this.user = m.data.user;
+        
     },
     computed: {
         headers () {
@@ -150,9 +231,17 @@ export default ({
           { text: 'Fat (g)', value: 'fats' },
           { text: 'Carbs (g)', value: 'carbs' },
           { text: 'Protein (g)', value: 'proteins' },
-          { text: 'Actions', value: 'actions', sortable: false }
+          { text: 'Status', value: 'actions', sortable: false }
         ]
+      },
+      tab: {
+      set (tab) {
+        this.$router.replace({ query: { ...this.$route.query, tab } })
+      },
+      get () {
+        return this.$route.query.tab
       }
+    }
     }
 })
 </script>
