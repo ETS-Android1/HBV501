bb4dba8286a2404abfcd810dc5e0cc400ce30a00
Anna Jaerving <annajrving@gmail.com>
2021-11-23 19:07:56 +0000

    sql typo

--- feedme/src/main/java/is/hi/feedme/repository/RecipeRepository.java
+++ feedme/src/main/java/is/hi/feedme/repository/RecipeRepository.java
@@ -299,9 +299,8 @@ public interface RecipeRepository extends JpaRepository<Recipe, Long> {
                         + "(SELECT recipe_id, COUNT(ingredient_id) FROM ingredient_quantity WHERE ingredient_id IN :ids GROUP BY recipe_id HAVING COUNT(ingredient_id) >= :size) t) AND"
                         + "(calories > :minCalories AND calories < :maxCalories) AND "
                         + "(carbs > :minCarbs AND carbs < :maxCarbs) AND "
-                        + "(proteins > :minProteins AND proteins < :maxProteins) AND"
-                        + "(fats > :minFats AND fats < :maxFats)"
-                        + "tbl1 LEFT JOIN (SELECT recipe_id, AVG(rating) FROM reviews GROUP BY recipe_id) tbl2 ON tbl1.id = tbl2.recipe_id) "
+                        + "(proteins > :minProteins AND proteins < :maxProteins) AND "
+                        + "(fats > :minFats AND fats < :maxFats)) tbl1 LEFT JOIN (SELECT recipe_id, AVG(rating) FROM reviews GROUP BY recipe_id) tbl2 ON tbl1.id = tbl2.recipe_id) "
                         + "ORDER BY avg DESC NULLS LAST", nativeQuery = true)
         List<Recipe> findByIngredientIdsSortedByRating(@Param("ids") List<Long> recipeIdsList, @Param("size") int size,
                         @Param("minCalories") int minCalories, @Param("maxCalories") int maxCalories,
@@ -327,9 +326,9 @@ public interface RecipeRepository extends JpaRepository<Recipe, Long> {
                         + "(SELECT recipe_id, COUNT(ingredient_id) FROM ingredient_quantity WHERE ingredient_id IN :ids GROUP BY recipe_id HAVING COUNT(ingredient_id) >= :size) t) AND"
                         + "(calories > :minCalories AND calories < :maxCalories) AND "
                         + "(carbs > :minCarbs AND carbs < :maxCarbs) AND "
-                        + "(proteins > :minProteins AND proteins < :maxProteins) AND"
-                        + "(fats > :minFats AND fats < :maxFats)"
-                        + "tbl1 LEFT JOIN (SELECT recipe_id, AVG(rating) FROM reviews GROUP BY recipe_id) tbl2 ON tbl1.id = tbl2.recipe_id) "
+                        + "(proteins > :minProteins AND proteins < :maxProteins) AND "
+                        + "(fats > :minFats AND fats < :maxFats)) tbl1 "
+                        + "LEFT JOIN (SELECT recipe_id, AVG(rating) FROM reviews GROUP BY recipe_id) tbl2 ON tbl1.id = tbl2.recipe_id) "
                         + "ORDER BY avg DESC NULLS LAST LIMIT :limit OFFSET :offset", nativeQuery = true)
         List<Recipe> findByIngredientIdsSortedByRatingPaginated(@Param("ids") List<Long> recipeIdsList,
                         @Param("size") int size, @Param("limit") int limit, @Param("offset") int offset,
