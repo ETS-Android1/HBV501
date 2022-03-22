471c45934da89fea103c43f3b5f8ceabf21000f5
Anna Jaerving <AnnaJrving@gmail.com>
2022-03-22 16:30:57 +0000

    wake me up, wake me up inside

---
+++ feedme-android/app/src/main/java/hi/feedme/feedme/ui/recipe/ExpandableIngredientListAdapter.java
@@ -0,0 +1,95 @@
+package hi.feedme.feedme.ui.recipe;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.TextView;
+import java.util.HashMap;
+import java.util.List;
+
+import hi.feedme.feedme.R;
+import hi.feedme.feedme.models.Ingredient;
+import hi.feedme.feedme.models.Recipe;
+
+public class ExpandableIngredientListAdapter extends BaseExpandableListAdapter {
+    private Context context;
+    private List<String> expandableTitleList;
+    private HashMap<String, List<Ingredient>> expandableDetailList;
+
+    public ExpandableIngredientListAdapter(Context context, List<String> expandableListTitle,
+                                           HashMap<String, List<Ingredient>> expandableListDetail) {
+        this.context = context;
+        this.expandableTitleList = expandableListTitle;
+        this.expandableDetailList = expandableListDetail;
+    }
+
+    @Override
+    public Object getChild(int lstPosn, int expanded_ListPosition) {
+        return this.expandableDetailList.get(this.expandableTitleList.get(lstPosn)).get(expanded_ListPosition);
+    }
+
+    @Override
+    public long getChildId(int listPosition, int expanded_ListPosition) {
+        return expanded_ListPosition;
+    }
+
+    @Override
+    public View getChildView(int lstPosn, final int expanded_ListPosition,
+                             boolean isLastChild, View convertView, ViewGroup parent) {
+        final String expandedListText = ((Ingredient) getChild(lstPosn, expanded_ListPosition)).getIngredient().getName();
+        if (convertView == null) {
+            LayoutInflater layoutInflater = (LayoutInflater) this.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = layoutInflater.inflate(R.layout.ingredient_list_item, null);
+        }
+        TextView expandedListTextView = (TextView) convertView.findViewById(R.id.expanded_ingredient);
+        expandedListTextView.setText(expandedListText);
+        return convertView;
+    }
+
+    @Override
+    public int getChildrenCount(int listPosition) {
+        return this.expandableDetailList.get(this.expandableTitleList.get(listPosition)).size();
+    }
+
+    @Override
+    public Object getGroup(int listPosition) {
+        return this.expandableTitleList.get(listPosition);
+    }
+
+    @Override
+    public int getGroupCount() {
+        return this.expandableTitleList.size();
+    }
+
+    @Override
+    public long getGroupId(int listPosition) {
+        return listPosition;
+    }
+
+    @Override
+    public View getGroupView(int listPosition, boolean isExpanded, View convertView, ViewGroup parent) {
+        String listTitle = (String) getGroup(listPosition);
+        if (convertView == null) {
+            LayoutInflater layoutInflater = (LayoutInflater) this.context.
+                    getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = layoutInflater.inflate(R.layout.ingredient_list_group, null);
+        }
+        TextView listTitleTextView = (TextView) convertView.findViewById(R.id.ingredient_name);
+        listTitleTextView.setTypeface(null, Typeface.BOLD);
+        listTitleTextView.setText(listTitle);
+        return convertView;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    @Override
+    public boolean isChildSelectable(int listPosition, int expandedListPosition) {
+        return true;
+    }
+}
--- feedme-android/app/src/main/java/hi/feedme/feedme/ui/recipe/RecipeFragment.java
+++ feedme-android/app/src/main/java/hi/feedme/feedme/ui/recipe/RecipeFragment.java
@@ -7,14 +7,23 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ExpandableListAdapter;
+import android.widget.ExpandableListView;
 import android.widget.TextView;
 
-import com.google.android.material.slider.RangeSlider;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 import hi.feedme.feedme.R;
+import hi.feedme.feedme.models.Ingredient;
 import hi.feedme.feedme.models.Recipe;
 
 public class RecipeFragment extends Fragment {
+    private ExpandableListView expandableIngredientListView;
+    private ExpandableListAdapter expandableIngredientListAdapter;
+    private List<String> expandableIngredientTitleList;
+    private HashMap<String, List<Ingredient>> expandableIngredientDetailList;
     private Recipe shownRecipe;
 
     public RecipeFragment() {
@@ -38,7 +47,15 @@ public class RecipeFragment extends Fragment {
         TextView txt = view.findViewById(R.id.recipe_name);
 
         txt.setText(shownRecipe.getName());
-        System.out.println(shownRecipe.getName());
+
+        ArrayList<Ingredient> ingredients = shownRecipe.getIngredients();
+
+        expandableIngredientListView = (ExpandableListView) view.findViewById(R.id.ingredients_list);
+        expandableIngredientDetailList = new HashMap<String, List<Ingredient>>();
+        expandableIngredientDetailList.put("Ingredients", ingredients);
+        expandableIngredientTitleList = new ArrayList<String>(expandableIngredientDetailList.keySet());
+        expandableIngredientListAdapter = new ExpandableIngredientListAdapter(view.getContext(), expandableIngredientTitleList, expandableIngredientDetailList);
+        expandableIngredientListView.setAdapter(expandableIngredientListAdapter);
 
         return view;
     }
