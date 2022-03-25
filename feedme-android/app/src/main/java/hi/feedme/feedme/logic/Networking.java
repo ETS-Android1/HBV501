35ac1b100e7c1c76e2358dd6cc7693e8228fff40
vallzy <45981588+vallzy@users.noreply.github.com>
2022-03-25 01:22:37 +0000

    postFavorite + comments
    Added the post favorite function, as well as finishing commenting.

--- feedme-android/app/src/main/java/hi/feedme/feedme/logic/Networking.java
+++ feedme-android/app/src/main/java/hi/feedme/feedme/logic/Networking.java
@@ -1,8 +1,8 @@
 package hi.feedme.feedme.logic;
 
 import android.content.Context;
-import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
@@ -14,19 +14,20 @@ import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 import hi.feedme.feedme.listeners.IngredientListNwCallback;
 import hi.feedme.feedme.listeners.LoginNwCallback;
 import hi.feedme.feedme.listeners.RecipeListNwCallback;
 import hi.feedme.feedme.listeners.RecipeNwCallback;
 import hi.feedme.feedme.listeners.RegisterNwCallback;
-import hi.feedme.feedme.models.Ingredient;
 import hi.feedme.feedme.models.IngredientInfo;
+import hi.feedme.feedme.models.LoginInformation;
 import hi.feedme.feedme.models.Recipe;
 import hi.feedme.feedme.models.SimplifiedRecipe;
 
 public class Networking {
-    // Database stuff
+    // REST
     private Context context;
     public static final String ROOT = "https://obscure-headland-63204.herokuapp.com/";
 
@@ -35,14 +36,19 @@ public class Networking {
     }
 
 
-    //GET
-
+    /**
+     * Fetches a recipe from the backend, to be returned in the callback
+     * @param id the target id of the recipe
+     * @param nwcb instance of the callback listener when fetching has finished
+     */
     public void getRecipeById(String id, RecipeNwCallback nwcb) {
         String url = ROOT + "recipes/" + id;
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
                 (Request.Method.GET, url, null, response -> {
                     try {
+                        //Parsing the json -> Recipe object
                         Recipe r = JSONParser.parseRecipe(response.toString());
+                        //Returning the Recipe object via notifying the listener.
                         nwcb.notifySuccess(r);
                     } catch (JsonProcessingException e) {
                         e.printStackTrace();
@@ -51,9 +57,14 @@ public class Networking {
                     System.out.println("Error!: " + error.toString());
                     nwcb.notifyError(error);
                 });
-        ReqQueue.getInstance(context).addToRequestQueue(jsonObjectRequest);
+        ReqQueue.getInstance(context).addToRequestQueue(jsonObjectRequest); //Sending the HTTP request
     }
 
+    /**
+     * Fetches all the recipes from the backend satisfying the query, to be returned in the callback
+     * @param q query to be used for the Recipe filtering
+     * @param nwcb instance of the callback listener when fetching has finished
+     */
     public void getRecipes(RecipeListNwCallback nwcb, String q) {
         String url = ROOT + "recipes" + q;
 
@@ -61,12 +72,15 @@ public class Networking {
             (Request.Method.GET, url, null, response -> {
                 try {
                     ArrayList<SimplifiedRecipe> recipeList = new ArrayList();
+                    //Getting all of the JSON from the JSON recipes array
                     JSONArray recipes = response.getJSONArray("recipes");
                     for(int i = 0; i < recipes.length(); i++) {
+                        //Getting i-th recipe json, parsing it to a model and adding it to the return list.
                         String js = recipes.getJSONObject(i).toString();
                         SimplifiedRecipe r = JSONParser.parseSimplifiedRecipe(js);
                         recipeList.add(r);
                     }
+                    //Returning the SimplifiedRecipe list to the listener.
                     nwcb.notifySuccess(recipeList);
                 } catch (JSONException | JsonProcessingException e) {
                     e.printStackTrace();
@@ -78,12 +92,18 @@ public class Networking {
         ReqQueue.getInstance(context).addToRequestQueue(jsonObjectRequest);
     }
 
+    /**
+     * Fetches all the ingredients from the backend currently in use, to be returned in the callback
+     * @param nwcb instance of the callback listener when fetching has finished
+     */
     public void getIngredients(IngredientListNwCallback nwcb) {
         String url = ROOT + "ingredients";
+        //Getting the ingredients JSON array
         JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, url, null, response -> {
             ArrayList<IngredientInfo> ingList = new ArrayList<>();
             for(int i=0;i<response.length();i++) {
                 try {
+                    //Getting i-th ingredient info, parsing it and adding it to the return list
                     JSONObject ing = response.getJSONObject(i);
                     IngredientInfo inginfo = JSONParser.parseIngredientInfo(ing.toString());
                     ingList.add(inginfo);
@@ -92,6 +112,7 @@ public class Networking {
                 }
             }
             try {
+                //Returning the IngredientInfo list to the listener.
                 nwcb.notifySuccess(ingList);
             } catch (JsonProcessingException e) {
                 e.printStackTrace();
@@ -103,14 +124,24 @@ public class Networking {
 
     //POST
 
+    /**
+     * Attempts to login the user with a username and password combination.
+     * Returns LoginInformation from the callback upon a successful login.
+     * @param user username of the login request
+     * @param pw password of the login request
+     * @param nwcb instance of the callback listener, returning LoginInformation or an error.
+     */
     public void postLogin(String user, String pw, LoginNwCallback nwcb) {
         String url = ROOT + "users/login";
+        //Create HashMap to handle our parameters that are sent with the HTTP request
         HashMap<String, String> params = new HashMap<String, String>();
         params.put("username", user);
         params.put("password", pw);
         JsonObjectRequest req = new JsonObjectRequest(Request.Method.POST, url, new JSONObject(params), response -> {
             try {
+                //If login is successful, store our token and login information in the storage
                 Storage.setJsonLogin(context, response.toString());
+                //Return the LoginInformation object of the logged in user.
                 nwcb.notifySuccess(Storage.getLoginInformation(context));
             } catch (JsonProcessingException e) {
                 e.printStackTrace();
@@ -119,19 +150,64 @@ public class Networking {
         ReqQueue.getInstance(context).addToRequestQueue(req);
     }
 
+    /**
+     * Attempts to register the user with a username, password and email combination.
+     * @param user username of the register request
+     * @param pw password of the register request
+     * @param email email of the register request
+     * @param admin indicates whether the register should be treated as an admin or not
+     * @param nwcb instance of the callback listener
+     */
     public void postRegister(String user, String pw, String email, Boolean admin, RegisterNwCallback nwcb) {
         String url = ROOT + "users/register";
+        //Create HashMap to handle our parameters that are sent with the HTTP request
         HashMap<String, String> params = new HashMap<String, String>();
         params.put("username", user);
         params.put("password", pw);
         params.put("email", email);
         params.put("admin", admin.toString());
         JsonObjectRequest req = new JsonObjectRequest(Request.Method.POST, url, new JSONObject(params), response -> {
+            //If the register is successful, notify the listener.
             nwcb.notifySuccess("Success");
         }, nwcb::notifyError);
         ReqQueue.getInstance(context).addToRequestQueue(req);
     }
 
+    /**
+     * Favorites a given recipe, provided the user is logged in.
+     * @param recipeId id of the recipe that's being favorited
+     */
+    public void postFavorite(String recipeId) {
+        LoginInformation current = null;
+        try {
+            current = Storage.getLoginInformation(context);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        if(current == null) return; //User isn't logged or has an expired token
+        String url = ROOT + "users/me/recipes/" + recipeId;
+        JsonObjectRequest req = new JsonObjectRequest(Request.Method.POST, url, null, response -> {
+
+        }, error -> {})
+        {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                //We need to override the headers in order to place our custom authorization
+                String token = "";
+                try {
+                    token = Storage.getLoginInformation(context).getToken();
+                } catch (JsonProcessingException e) {
+                    e.printStackTrace();
+                }
+                HashMap<String, String> headers = new HashMap<>();
+                //Adhering to the JWT standard
+                headers.put("Content-type","application/json");
+                headers.put("Authorization", "Bearer " + token);
+                return headers;
+            }
+        };
+        ReqQueue.getInstance(context).addToRequestQueue(req);
+    }
 
 }
 
--- feedme-android/app/src/main/java/hi/feedme/feedme/logic/Storage.java
+++ feedme-android/app/src/main/java/hi/feedme/feedme/logic/Storage.java
@@ -14,19 +14,38 @@ public class Storage {
 
     private Storage() {}
 
+    /**
+     * The main storage get-method.
+     * @param context the context of the request
+     */
     private static SharedPreferences getSharedPreferences(Context context) {
         return context.getSharedPreferences(APP_TOKEN, Context.MODE_PRIVATE);
     }
 
+    /**
+     * Gets our login information in JSON form
+     * @param context the context of the request
+     */
     public static String getJsonLogin(Context context) {
         return getSharedPreferences(context).getString(LOGIN_INFORMATION, null);
     }
 
+    /**
+     * Saves a json token to the  stored login information
+     * @param context the context of the request
+     * Returns our json login information in a LoginInformation object
+     * @throws JsonProcessingException If the json to LoginInformation parsing is invalid or unsuccessful.
+     */
     public static LoginInformation getLoginInformation(Context context) throws JsonProcessingException {
         String json = getJsonLogin(context);
         return JSONParser.parseLogin(json);
     }
 
+    /**
+     * Saves a json token to the  stored login information
+     * @param context the context of the request
+     * @param token the JWT token from the login attempt
+     */
     public static void setJsonLogin(Context context, String token) {
         final SharedPreferences.Editor editor = getSharedPreferences(context).edit();
         editor.putString(LOGIN_INFORMATION, token);
